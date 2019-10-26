5cb3eb06e7436b17debe02bd82c74e9038458383
Jorge A Esguerra A <ja.esguerra@uniandes.edu.co>
2019-10-26 02:38:16 -0500

    Este commit se tira todo

--- HackF/campesinos/models.py
+++ HackF/campesinos/models.py
@@ -6,6 +6,6 @@ class Campesino(models.Model):
     #productos = ???
     nombre = models.CharField(max_length = 200)
     ubicacion = models.CharField(max_length = 200)
-
+    
     def __str__(self):
         return self.nombre
--- hackF_back/productos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.views.decorators.csrf import csrf_exempt
-
-urlpatterns = {
-  path('', views.producto_list, name='producto_list'),
-  path('new' csrf_exempt(views.producto_create), name='producto_new')
-}
