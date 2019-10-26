f0d2fcb9a9ce90f320b722d9d987c4d788595555
root <root@DESKTOP-OETNJRA.localdomain>
2019-10-26 01:42:41 -0500

    Merged apps and changes

--- HackF/HackF/settings.py
+++ HackF/HackF/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'campesinos.apps.CampesinosConfig',
-    'productos.apps.ProductosConfig'
+    'productos.apps.ProductosConfig',
+    'ofertas.apps.'
 
 ]
 
--- HackF/ofertas/apps.py
+++ HackF/ofertas/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class OfertaConfig(AppConfig):
-    name = 'oferta'
+class OfertasConfig(AppConfig):
+    name = 'ofertas'
