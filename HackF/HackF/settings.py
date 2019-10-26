029aad4d8010a1bd2b14d127ec404d8298e87663
Jorge A Esguerra A <ja.esguerra@uniandes.edu.co>
2019-10-26 01:27:44 -0500

    finished campesinos and ofertas

--- HackF/HackF/settings.py
+++ HackF/HackF/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'campesinos.apps.CampesinosConfig'
+    'campesinos.apps.CampesinosConfig',
+    'ofertas.apps.'
 ]
 
 MIDDLEWARE = [
